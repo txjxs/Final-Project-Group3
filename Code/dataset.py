5bc9216b2f96b05d2988735e5fc32e0b24a85bcc
Sarfarazzzzz <m.shaik@gwu.edu>
2025-11-27 13:02:41 -0500

    Script for Dataset

--- Code/dataset.py
+++ Code/dataset.py
@@ -1,30 +1,39 @@
 import os
 from PIL import Image
 from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+from utils import preprocess_image  # Import the helper function
 
-class COCODataset(Dataset):
-    """
-    Custom class to load and preprocess COCO dataset
-    """
-    def __init__(self,root_dir, transform=None):
+
+class ColorizationDataset(Dataset):
+    def __init__(self, root_dir, split='train'):
         self.root_dir = root_dir
-        self.transform = transform
+        self.image_files = [f for f in os.listdir(root_dir) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
 
-        self.image_files = [f for f in os.listdir(root_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
-        print(f"Found {len(self.image_files)} images in {root_dir}")
+        # Basic transforms (Resize, Augmentation)
+        if split == 'train':
+            self.transform = transforms.Compose([
+                transforms.Resize((256, 256)),
+                transforms.RandomHorizontalFlip(),
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.Resize((256, 256))
+            ])
 
     def __len__(self):
         return len(self.image_files)
 
     def __getitem__(self, idx):
-        image_name = self.image_files[idx]
-        image_path = os.path.join(self.root_dir, image_name)
+        img_path = os.path.join(self.root_dir, self.image_files[idx])
 
-        image = Image.open(image_path).convert('RGB')
+        # 1. Open image
+        img = Image.open(img_path).convert("RGB")
 
-        if self.transform:
-            image = self.transform(image)
-            
-        return image
+        # 2. Use our Utility function to "decolorize" and split channels
+        # This returns normalized L (input) and ab (target) tensors
+        L, ab = preprocess_image(img, self.transform)
+
+        return L, ab
 
 
